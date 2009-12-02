d742c566bb1d3ae0632fe9d0965340b322b39b85
jdi613 <jdi613@002ad63e-63f3-0310-a63f-d79a47de8aeb>
2009-12-02 17:46:49 +0000

    Change whoami controller to pull the user from the correct place when proxy tickets are used.
    git-svn-id: svn+ssh://code/svnrep/projects/coldfusion_cas_client@18842 002ad63e-63f3-0310-a63f-d79a47de8aeb

--- test_service/app/controllers/whoami_controller.rb
+++ test_service/app/controllers/whoami_controller.rb
@@ -1,6 +1,13 @@
 class WhoamiController < ApplicationController
   def show
-    @username = session[:bcsec_user].username
+    @username = 
+      if session[:bcsec_user]
+        session[:bcsec_user].username
+      elsif @bcsec_cas_proxy_user
+        @bcsec_cas_proxy_user.username
+      else
+        'NO USER SET!!!'
+      end
     respond_to do |format|
        format.html 
        format.xml  { render :xml => "<?xml version=\"1.0\" ?><user><name>#{@username}</name></user>" }
